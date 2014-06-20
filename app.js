477e9aa61bfe9100fa211ce1baf52a77b2de96f2
James Vo <quangvo55@gmail.com>
2014-06-20 09:57:51 -0700

    db added

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var app = express();
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-	host : 'localhost',
-	user : 'root',
-	password: '',
-	database: 'test'
+	host : 'us-cdbr-east-06.cleardb.net',
+	user : 'b61ee1d3f83069',
+	password: '03c9629a',
+	database: 'heroku_94e96de1b6e2705'
 });
 
 connection.connect(function(err) {
